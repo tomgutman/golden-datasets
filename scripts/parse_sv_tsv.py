c8fae4fc980dfc660a526d132ef36da2704aaa61
aferriz <alvaro.ferriz@hotmail.com>
2020-12-22 09:24:19 +0100

    [MODIF] Some additions

--- scripts/parse_sv_tsv.py
+++ scripts/parse_sv_tsv.py
@@ -1,3 +1,4 @@
+import sys
 
 def parse(vcf_reader):
     variants = []
@@ -5,54 +6,69 @@ def parse(vcf_reader):
     nr_filtered = 0
     ref_version = "Unknown"
     header = False
-    min_nr_progs = 2    # Define the number of programs that need to call the variant for it to continue
-
+    #min_nr_progs = 2    # Define the number of programs that need to call the variant for it to continue
+    
     for line in vcf_reader:
         line = line.rstrip().split()
-
+       
         if not header:
             # Try finding a reference genome file
-            if line[0] == "##Reference":
+            if "##reference" in line[0]:
                 ref_version = "\t".join(line)
-
-            # Parse in header line for BSC, Charite
-            if line[0] in ["CHR1", "#chrom1"]:
+                
+            # Parse in header line
+            if line[0] == "CHR1" or line[0] == "#chrom1": #BSC and Charite support
                 header = line
-                print("[INFO] Header of TSV file found.")
-                print(header)
-
+            
+            # If variants are reached without column names been found, stop execution
+            if line[0] == "1" or line[0] == "chr1":
+                if header == False:
+                    sys.exit("[ERROR] Column names not found. Exiting.")
+                
             continue
-
+    
         nr_of_vars += 1
-
-        # Filter out variants with filter tag (BSC)
-        if 'CUSTOM_FILTER' in header:
-            if line[header.index('CUSTOM_FILTER')] != "PASS":
+        
+        # Different processing depending if it's BSC file or Charite file
+        if "FILTER" in header: # Charite does not have FILTER column so it does not apply. We need to ask them.
+            
+            # Filter out variants with filter tag
+            if line[header.index('FILTER')] != "PASS":
                 #print("[DEBUG] Found filter: " + str(line[header.index('CUSTOM_FILTER')]))
                 nr_filtered += 1
+                continue #This makes to go to the next line and forget about this variant
+                
+                #Maybe we could recover these discarded variants in another dataframe...
+    
+            '''
+            # Filter out variants that do not get called by min_nr_progs
+            if int(line[header.index('NPROGS')]) < min_nr_progs:
+                nr_filtered += 1
                 continue
-
-        '''
-        # Filter out variants that do not get called by min_nr_progs
-        if int(line[header.index('NPROGS')]) < min_nr_progs:
-            nr_filtered += 1
-            continue
-        '''
-
-        # BSC TSV support
-        if 'CHR1' in header:
+            '''
+            # If .tsv is from BSC:
             start_chrom = line[header.index('CHR1')]
             start = line[header.index('POS1')]
             end_chrom = line[header.index('CHR2')]
             end = line[header.index('POS2')]
-            ref = None
-            alt = None
-            if start_chrom == end_chrom:
-                length = int(end) - int(start)
-            else:
-                length = None
-
-            # Find sv_type
+        
+        else: # If .tsv is from Charite:
+            start_chrom = line[header.index('#chrom1')]
+            start = line[header.index('start1')]
+            end_chrom = line[header.index('chrom2')]
+            end = line[header.index('start2')]
+    
+        ref = None
+        alt = None  
+        
+        if start_chrom == end_chrom:
+            length = int(end) - int(start)
+        else:
+            length = None #Only for SV involving different chr.. maybe use sth more specific.
+    
+        # Find sv_type
+        # If .tsv is from BSC:
+        if "FILTER" in header:
             svtypes = []
             progs = line[header.index('PROGS')]
             for prog in progs.split(","):
@@ -82,27 +98,24 @@ def parse(vcf_reader):
                     print("Unknown SV combination! Please check!")
                     print(svtypes)
                     sv_type = None
-
-            # Reformat types
-            sv_type.replace("deletion", "DEL")
-
-        # Charite support
-        elif '#chrom1' in header:
-            start_chrom = line[header.index('#chrom1')]
-            start = line[header.index('start1')]
-            end_chrom = line[header.index('chrom2')]
-            end = line[header.index('start2')]
-            ref = None
-            alt = None
-            if start_chrom == end_chrom:
-                length = int(end) - int(start)
-            else:
-                length = None
-            sv_type = line[header.index('svtype')]
+    
+        # If .tsv is from Charite:
+        else: 
+            sv_type=line[header.index('svtype')]
+    
+    
+    
+        # Reformat types
+        
+        sv_type = sv_type.replace("deletion", "DEL")
+        sv_type = sv_type.replace("inversion", "INV")
+        sv_type = sv_type.replace("translocation", "TRA")
+        sv_type = sv_type.replace("insertion", "INS")
+        sv_type = sv_type.replace("insertion", "INS")
+    
         #print([start_chrom, start, end_chrom, end, ref, alt, length, sv_type])
-
         variants.append([start_chrom, start, end_chrom, end, ref, alt, length, sv_type])
-
+    
     print("[INFO] Reference version: " + ref_version)
-
+    
     return(variants, nr_of_vars, nr_filtered)
