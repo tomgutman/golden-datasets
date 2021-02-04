3fe33d1c92921094fa57870fa1362ad09c6fa2ce
Tom <tom.gutman@curie.fr>
2021-02-04 12:06:14 +0100

    [MODIF] create new dataframe for indels > 50bp + remove index

--- scripts/parse_snv.py
+++ scripts/parse_snv.py
@@ -8,6 +8,7 @@ def parse(vcf_reader, samplename):
 
     variants_snv = []
     variants_indel = []
+    variants_sv = []
     sample = None
     nr_of_vars = 0
     nr_filtered = 0
@@ -37,11 +38,17 @@ def parse(vcf_reader, samplename):
             chrom = record.CHROM
             pos = record.POS
             ref = record.REF
-            alt = record.ALT
+            alt = record.ALT[0]
 
             if record.var_type == "indel":
-                variants_indel.append([chrom, pos, ref, alt])
+                len_ref = len(ref)
+                len_alt = len(alt)
+                if len_ref > 50 or len_alt > 50:
+                    variants_sv.append([chrom, pos, ref, alt,len_ref,len_alt])
+                else:
+                    variants_indel.append([chrom, pos, ref, alt])
                 #print("indel: {} {} {}".format(pos,ref,alt))
+                #print(len(ref),len(alt))
 
             elif record.var_type == "snp":
                 #print ("snp: {} {} {}".format(pos,ref,alt))
@@ -57,4 +64,4 @@ def parse(vcf_reader, samplename):
 
     #print(variants)
 
-    return(variants_snv, variants_indel, nr_of_vars, nr_filtered)
+    return(variants_snv, variants_indel, variants_sv, nr_of_vars, nr_filtered)
