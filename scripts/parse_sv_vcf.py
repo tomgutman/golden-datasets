2c1f1fd8e6779eb8af32475e5ecdc9712988df5b
Daphne van Beek <d.vanbeek@hartwigmedicalfoundation.nl>
2021-02-11 10:24:17 +0100

    Added support for EVENTTYPE for Hartwig SV VCF

--- scripts/parse_sv_vcf.py
+++ scripts/parse_sv_vcf.py
@@ -99,12 +99,9 @@ def parse(vcf_reader, samplename):
             alt = record.ALT[0]
 
             sv_type = None
-            '''
             if 'EVENTTYPE' in record.INFO: # Support Hartwig
                 sv_type = record.INFO['EVENTTYPE']
-                print(sv_type)
-            '''
-            if 'SVTYPE' in record.INFO: # Support Curie
+            elif 'SVTYPE' in record.INFO: # Support Curie
                 sv_type = record.INFO['SVTYPE']
             else:
                 sv_type = record.var_subtype
