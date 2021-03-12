82545dc007c3f648c232775be23b04c06e90e7b8
Daphne van Beek <d.vanbeek@hartwigmedicalfoundation.nl>
2021-03-12 10:25:22 +0100

    Added SV benchmarking to bash script

--- scripts/run_batch.sh
+++ scripts/run_batch.sh
@@ -8,6 +8,7 @@ function run {
     SV_VCF=$3
     samplename=$4
     out_dir=$5
+    truth_sv_df=$6
 
     out_snv_df=${out_dir}"/"${prefix}"_snv_df.csv"
     out_sv_df=${out_dir}"/"${prefix}"_sv_df.csv"
@@ -18,7 +19,7 @@ function run {
 
     # For Tom: conditional test in bash script to test if the input dataframes are empty.
     python ${repo}/reclassification.py ${out_snv_df} ${out_sv_df} ${reclass_snv_df} ${reclass_sv_df}
-    
+    python ${repo}/compare_node_to_truth.py ${reclass_sv_df} ${truth_sv_df}
     
     
 
@@ -26,7 +27,7 @@ function run {
 
 
 # Hartwig
-run ${repo_path} "Hartwig" "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/vcfs/Hartwig/5.19/COLO829v003T.purple.sv.vcf.gz" "COLO829v003T" "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/dataframes/"
+run ${repo_path} "Hartwig" "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/vcfs/Hartwig/5.19/COLO829v003T.purple.sv.vcf.gz" "COLO829v003T" "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/dataframes/" "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/dataframes/truth.csv"
 
 # BSC
 #run ${repo_path} "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/vcfs/BSC_SV_COLO.tsv" "none" "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/dataframes/bsc.csv"
