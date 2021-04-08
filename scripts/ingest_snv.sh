b4266c7c0a1c6f4ec481d8b430c1cdfd009fcbd1
Tom <tom.gutman@curie.fr>
2021-04-08 11:07:29 +0200

    [UPDATE] ingesy snv

--- scripts/ingest_snv.sh
+++ scripts/ingest_snv.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+set -e
 #EUCANCAN SNV & INDEL vcf handling
+KEEP=false
 
-while getopts "t:s:i:v:f:o:n:c:h" option; do
+while getopts "t:s:i:v:f:o:n:kh" option; do
     case "${option}" in
         t) truth=${OPTARG};;
         s) snv=${OPTARG};;
@@ -12,7 +13,7 @@ while getopts "t:s:i:v:f:o:n:c:h" option; do
         f) HGREF=${OPTARG};;
         o) OUTPUT_DIR=${OPTARG};;
         n) SAMPLE_NAME=${OPTARG};;
-        c) CONFIG=${OPTARG};;
+        k) KEEP=true;;
         h)  echo "Usage:"
             echo "bash tmb_dragon.sh -t truth_file.vcf"
             echo "                   -s snv.vcf"
@@ -22,7 +23,7 @@ while getopts "t:s:i:v:f:o:n:c:h" option; do
             echo "                   -f ref_fasta.fa"
             echo "                   -o /OUTPUT_DIR/PATH"
             echo "                   -n Sample name"
-            echo "                   -c config_file.txt"
+            echo "                   -k (to keep intermediates files)"
             exit
     esac
 done
@@ -35,67 +36,97 @@ echo "INDEL vcf file:" $indel
 echo "SV vcf file:" $sv
 echo "TRUTH SV file:" $truth_sv
 echo "Reference fasta file:" $FASTA
-echo "output path": $OUTPUT_DIR
+echo "output path:" $OUTPUT_DIR
 echo "sample Name:" $SAMPLE_NAME
-echo "config file:" $CONFIG
+echo "keep intermediate files ?:" $KEEP
 echo " "
 
-#Load Config file:
-source ${CONFIG}
+# Create output dir:
+
+mkdir -p $OUTPUT_DIR/$SAMPLE_NAME
+OUTPUT_DIR=$OUTPUT_DIR/$SAMPLE_NAME
 
 # Load conda env:
-#conda env create -n eucancan -f golden-datasets/scripts/environment.yml 
+#conda env create -n eucancan -f golden-datasets/scripts/environment.yml
 
 source activate eucancan
 
+# Check if vcf is mono sample:
+
+if [ `bcftools query -l $snv |wc -l` -gt 1 ] || [ `bcftools query -l $truth |wc -l` -gt 1 ]; then
+    echo $snv "is a multisample"
+    echo "the vcf must be single sample "
+    exit
+fi
+# bcftools view -c1 -Oz -s COLO829T -o curie_colo829_indels.sample.vcf curie_colo829_indels.vcf
+
+
+# if [ `$BCF_DIR/bcftools query -l $file |wc -l` -gt 1 ] ; then
+#     echo $ID "is a multisample"
+#     echo `$BCF_DIR/bcftools query -l $file`
+#     for sample_vcf in `$BCF_DIR/bcftools query -l $file`; do
+#         echo $sample_vcf
+#
+#         $BCF_DIR/bcftools view -c1 -Oz -s $sample_vcf -o $OUTPUT_DIR/$dataset/$sample_vcf"_multisample.hg19_multianno.vcf.gz" $file
+#         gunzip $OUTPUT_DIR/$dataset/$sample_vcf"_multisample.hg19_multianno.vcf.gz"
+
+
 # Sorting vcf files
 echo -e "[Running Information]: sorting vcf files\n"
-bcftools sort $snv -o `basename $snv .vcf`".sort.vcf.gz" -O z
-bcftools sort $indel -o `basename $indel .vcf`".sort.vcf.gz" -O z
+bcftools sort $snv -o $OUTPUT_DIR/`basename $snv .vcf.gz`".sort.vcf.gz" -O z
+bcftools sort $indel -o $OUTPUT_DIR/`basename $indel .vcf.gz`".sort.vcf.gz" -O z
+
+snv=`basename $snv .vcf.gz`".sort.vcf.gz"
+indel=`basename $indel .vcf.gz`".sort.vcf.gz"
 
 # indexing sorted vcf files
 echo -e "[Running Information]: indexing vcf files\n"
-bcftools index `basename $snv .vcf`".sort.vcf.gz"
-bcftools index `basename $indel .vcf`".sort.vcf.gz"
+bcftools index -f -o $OUTPUT_DIR/$snv".csi" $OUTPUT_DIR/$snv
+bcftools index -f -o $OUTPUT_DIR/$indel".csi" $OUTPUT_DIR/$indel
 
 # Merging SNV.vcf & INDEL.vcf:
 echo -e "[Running Information]: concatenate vcf files\n"
-bcftools concat -a `basename $snv .vcf`".sort.vcf.gz" `basename $indel .vcf`".sort.vcf.gz" -O z -o $OUTPUT_DIR/$SAMPLE_NAME"_merge.vcf.gz"
+bcftools concat -a $OUTPUT_DIR/$snv $OUTPUT_DIR/$indel -O z -o $OUTPUT_DIR/$SAMPLE_NAME"_merge.vcf.gz"
 
 # Preparing for normalization
 echo -e "[Running Information]: preparing for normalizing\n"
+echo -e "[Running Information]: indexing...]"
 
 #export $FASTA
 export HGREF=/data/annotations/pipelines/Human/hg19_base/genome/hg19_base.fa
 
-bcftools index $OUTPUT_DIR/$SAMPLE_NAME"_merge.vcf.gz"
-bcftools index $truth
+bcftools index -f -o $OUTPUT_DIR/$SAMPLE_NAME"_merge.vcf.gz.csi" $OUTPUT_DIR/$SAMPLE_NAME"_merge.vcf.gz"
+bcftools index -f $truth
 
+echo -e "[Running Information]: multimerge...]"
+
+echo $truth
+echo $OUTPUT_DIR/`basename $truth .vcf.gz`".prep.vcf.gz"
 multimerge $OUTPUT_DIR/$SAMPLE_NAME"_merge.vcf.gz" -r $HGREF -f true -o $OUTPUT_DIR/$SAMPLE_NAME"_merge.prep.vcf" --process-full=1
-multimerge $truth -r $HGREF -o `basename $truth .vcf.gz`".prep.vcf.gz" --process-full=1
+multimerge $truth -r $HGREF -o $OUTPUT_DIR/`basename $truth .vcf.gz`".prep.vcf.gz" --process-full=1
 
 # Normalizing vcfs:
 echo -e "[Running Information]: normalizing test file\n"
 merge_file=$OUTPUT_DIR/$SAMPLE_NAME"_merge.prep.vcf"
 
-pre.py $merge_file `basename $merge_file _merge.prep.vcf`".norm.vcf.gz" -L --decompose --somatic --pass-only
+pre.py $merge_file $OUTPUT_DIR/`basename $merge_file _merge.prep.vcf`".norm.vcf.gz" -L --decompose --somatic --pass-only
 
 echo -e "\n[Running Information]: normalizing truth file\n"
 
-truth=`basename $truth .vcf.gz`".prep.vcf.gz"
-pre.py $truth `basename $truth .vcf.gz`".norm.vcf.gz" -L --decompose --somatic --pass-only
+truth=$OUTPUT_DIR/`basename $truth .vcf.gz`".prep.vcf.gz"
+pre.py $truth $OUTPUT_DIR/`basename $truth .vcf.gz`".norm.vcf.gz" -L --decompose --somatic --pass-only
 
 # Running ingestion script:
 echo -e "[Running Information]: Running ingestion_snv.py script \n"
 
-python $SCRIPT_DIR/ingest_snv.py -samplename "SAMPLE" -o `basename $merge_file .vcf` `basename $merge_file _merge.prep.vcf`".norm.vcf.gz"
+python ~/Documents/Tom/EUCANCan/golden-datasets-fork-tom/golden-datasets/scripts/ingest_snv.py -samplename "SAMPLE" -o $OUTPUT_DIR/`basename $merge_file .vcf` $OUTPUT_DIR/`basename $merge_file _merge.prep.vcf`".norm.vcf.gz"
 
-python $SCRIPT_DIR/ingest_snv.py -samplename "SAMPLE" -o `basename $truth .vcf.gz` `basename $truth .vcf.gz`".norm.vcf.gz"
+python ~/Documents/Tom/EUCANCan/golden-datasets-fork-tom/golden-datasets/scripts/ingest_snv.py -samplename "SAMPLE" -o $OUTPUT_DIR/`basename $truth .vcf.gz` $OUTPUT_DIR/`basename $truth .vcf.gz`".norm.vcf.gz"
 
 # Running som.py:
 echo -e "[Running Information]: Running som.py evaluation script\n"
 
-som.py `basename $truth .vcf.gz`".filtered.vcf" `basename $merge_file .vcf`".filtered.vcf" -o `basename $merge_file .vcf`"eval.txt" --verbose -N
+som.py `basename $truth .vcf.gz`".filtered.vcf" $OUTPUT_DIR/`basename $merge_file .vcf`".filtered.vcf" -o $OUTPUT_DIR/`basename $merge_file .vcf`"eval.txt" --verbose -N
 
 echo -e "[Running Information]: script ended successfully\n"
 
@@ -105,7 +136,8 @@ echo -e "[Running Information]: Running ingestion SV script\n"
 
 # Cleaning
 echo -e "[Running Information]: Cleaning files\n"
-rm *{.tbi,csi,json}
+rm $OUTPUT_DIR/*{tbi,csi,json,vcf,vcf.gz}
+rm $truth".csi"
 
 #run ${repo_path} "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/vcfs/Hartwig/5.19/COLO829v003T.purple.sv.vcf.gz" "COLO829v003T" "/Users/daphnevanbeek/Projects/eucancan/COLO_RESULTS/dataframes/hartwig.csv"
 
@@ -144,3 +176,14 @@ rm *{.tbi,csi,json}
 #     -O test.multi.vcf \
 #     -V COLO829T_merge.vcf \
 #     --select-type-to-include SNP --select-type-to-include INDEL --exclude-filtered --select-type-to-exclude MNP
+
+# Running Dream data challenge:
+
+#SCRIPT_DIR=/bioinfo/users/tgutman/Documents/Tom/EUCANCan/golden-datasets-fork-tom/golden-datasets/scripts/
+#OUT_DIR=/bioinfo/users/tgutman/Documents/Tom/EUCANCan/Benchmark/test_ingestions_sh
+
+#bash $SCRIPT_DIR/ingest_snv.sh -t truth_dream/truth.snvs.synthetic.challenge.set1.chr.vcf.gz -s test_indel.vcf.gz -i test_snv.vcf.gz -o $OUT_DIR -c $SCRIPT_DIR/config.txt  -n curie_dream_1 -f /data/annotations/pipelines/Human/hg19_base/genome/hg19_base.fa
+
+
+#/!\ enlever le hard path du script python
+#/!\ ajouter etape de split multisample if it is the case
